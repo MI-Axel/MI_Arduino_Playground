cd4a8960054bc6d5ec6fb7e072daa5f9b164e4d6
Axel <axel@Axels-MacBook-Pro.local>
2019-02-10 21:56:00 +0800

    add keywords and license. the motor driver could run 2 motors seprately.

--- lib/MI_Curiosity_MotorShield/MI_Curiosity_MotorShield.h
+++ lib/MI_Curiosity_MotorShield/MI_Curiosity_MotorShield.h
@@ -23,16 +23,6 @@
 #include "utility/MI_Curiosity_MS_PWMServoDriver.h"
 
 //#define MOTORDEBUG
-
-#define MOTOR1_A 2
-#define MOTOR1_B 3
-#define MOTOR2_A 1
-#define MOTOR2_B 4
-#define MOTOR4_A 0
-#define MOTOR4_B 6
-#define MOTOR3_A 5
-#define MOTOR3_B 7
-
 #define FORWARD 1
 #define BACKWARD 2
 #define BRAKE 3
